2fa6a980fbf65b80477d7f3b31143738b30eaa36
Anand-1812 <anand.work2005@gmail.com>
2025-06-20 13:23:38 +0530

    feat: add attack method

--- src/players.js
+++ src/players.js
@@ -2,8 +2,13 @@ import { GameBoard } from "./gameBoard";
 
 // Player Class
 class Player {
-  constructor() {
+  constructor(name) {
     // each player will have a new gameboard
+    this.name = name;
     this.gameboard = new GameBoard();
+  }
+
+  attack(opponent, x, y) {
+    return opponent.gameboard.receiveAttack(x, y);
   }
 }
