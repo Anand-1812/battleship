99be5f36a8f51afa6793f1a8a973c319936cd3a3
Anand-1812 <anand.work2005@gmail.com>
2025-06-20 12:39:31 +0530

    Add: function to place the ships

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -3,12 +3,39 @@ import { Ship } from "./ship";
 // Game Board Class
 export class GameBoard {
   constructor() {
-    
+    this.grid = Array.from({ length: 10}, Array(10).fill(null));
+    this.ships = [];
   }
 
+  // places the ships in the grid
+  placeShips(x, y, length, direction) {
+    const ship = new Ship(length);
+    const coordinates = [];
 
+    for (let i = 0;i < length;i++) {
+      let xi = x;
+      let yi = y;
+
+      if (direction === 'horizontal') xi += 1;
+      else if (direction === 'vertical') yi += 1;
+
+      if (xi >= 10 || yi >= 10 || this.grid[xi][yi] !== NULL) {
+        throw new Error("Invalid ship placement.");
+      }
+
+      coordinates.push([xi, yi]);
+    }
+
+    coordinates.forEach(([xi, yi]) => {
+      this.grid[yi][xi] = ship;
+    });
+
+    this.ships.push(ship);
+  }
 
   recieveAttack(x, y) {
+    missedShots = [];
 
+    
   }
 }
