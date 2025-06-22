4ba29028c43f5492c3473163afff4fcefc7a65c8
Anand-1812 <anand.work2005@gmail.com>
2025-06-22 16:30:16 +0530

    fix: layout done

--- src/createGrid.js
+++ src/createGrid.js
@@ -1,7 +1,7 @@
 const userDiv = document.querySelector(".user-grid");
 const botDiv = document.querySelector(".bot-grid");
 
-export function createGrid(container, size = 10) {
+export function createGrid(container, size = 10, isUser = false) {
   for (let i = 0; i < size; i++) {
     const rowDiv = document.createElement("div");
     rowDiv.className = "gridRow";
@@ -9,13 +9,18 @@ export function createGrid(container, size = 10) {
     for (let j = 0; j < size; j++) {
       const colDiv = document.createElement("div");
       colDiv.className = "gridCol";
+      colDiv.dataset.row = i;
+      colDiv.dataset.col = j;
+
+      if (isUser) {
+        colDiv.addEventListener("click", () => {
+          placeShipoOnClick();
+        });
+      }
+
       rowDiv.appendChild(colDiv);
     }
 
     container.appendChild(rowDiv);
   }
 }
-
-// Make both grids
-createGrid(userDiv);
-createGrid(botDiv);
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import "./styles.css";
 import { createGrid } from "./createGrid.js";
 
 // create grid for both user and bot
-createGrid();
+createGrid(document.querySelector(".user-grid"), 10, true);
+createGrid(document.querySelector(".bot-grid"), 10, false);
+
