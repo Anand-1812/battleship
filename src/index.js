9647a9dd9b0aa60479a9839e5cf0d5d816cd4d28
Anand-1812 <anand.work2005@gmail.com>
2025-06-24 21:42:21 +0530

    feat: add randomize functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./styles.css";
-import { createGrid } from "./create_grid.js";
-import { Player } from "./players.js";
-import { randomlyPlaceShips } from "./game_board.js";
-import { renderBoard } from "./render_board.js";
-const userGrid = document.querySelector(".user-grid")
-const botGrid = document.querySelector(".bot-grid")
+import './styles.css';
+import { createGrid } from './create_grid.js';
+import { Player } from './players.js';
+import { randomlyPlaceShips } from './game_board.js';
+import { renderBoard } from './render_board.js';
+const userGrid = document.querySelector('.user-grid');
+const botGrid = document.querySelector('.bot-grid');
 
 // create grid for both user and bot
 createGrid(userGrid, 10, true);
@@ -12,13 +12,23 @@ createGrid(botGrid, 10, false);
 
 const user = new Player('User');
 
-randomlyPlaceShips(user.gameboard);
-renderBoard(user.gameboard.grid, userGrid);
+// randomly place ship
+const randomButton = document.querySelector('.randomize');
+randomButton.addEventListener('click', () => {
+  randomlyPlaceShips(user.gameboard);
+  renderBoard(user.gameboard.grid, userGrid);
+});
+
+const startButton = document.querySelector('.start');
+startButton.addEventListener('click', () => {
+  const displayTurn = document.querySelector('.display');
+  displayTurn.textContent = 'Your turn(attack)';
+});
 
 const bot = new Player('Bot');
 
 randomlyPlaceShips(bot.gameboard);
 
-bot.gameboard.getCoordinates(".bot-grid", (x, y) => {
+bot.gameboard.getCoordinates('.bot-grid', (x, y) => {
   user.attack(bot, x, y);
 });
