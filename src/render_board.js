049c6c9cddd0256c8d80081f328f66f55571a80e
Anand-1812 <anand.work2005@gmail.com>
2025-06-25 21:12:57 +0530

    some changes

--- src/render_board.js
+++ src/render_board.js
@@ -1,7 +1,7 @@
 export function renderBoard(grid, domGrid) {
-  for (let x = 0;x < 10;x++) {
-    for (let y = 0;y < 10;y++) {
-      const cell = domGrid.querySelector(`[data-row="${y}"][data-col="${x}"]`);
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      const cell = domGrid.querySelector(`[data-y="${y}"][data-x="${x}"]`);
       if (cell && grid[y][x] !== null) {
         cell.style.backgroundColor = '#D0D0D0';
       }
