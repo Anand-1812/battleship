58cde07837043c9a7ef4e1af7c905e93997cbba3
Anand-1812 <anand.work2005@gmail.com>
2025-06-27 17:03:43 +0530

    feat: seperate the user attack in a method

--- src/start_game.js
+++ src/start_game.js
@@ -1,19 +1,27 @@
 import { randomlyPlaceShips } from './game_board.js';
 
-export function startGame(user, bot) {
-  const display = document.querySelector('.display');
-  display.textContent = 'Your turn(attack)';
-  const botGrid = document.querySelector('.bot-grid');
+export class StartGame {
+  constructor(user, bot) {
+    this.user = user;
+    this.bot = bot;
+  }
 
-  randomlyPlaceShips(bot.gameboard);
-  bot.gameboard.getCoordinates('.bot-grid', (x, y) => {
-    const result = user.attack(bot, x, y);
-    const cell = botGrid.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+  userAttack() {
+    const display = document.querySelector('.display');
+    display.textContent = 'Your turn(attack)';
+    const botGrid = document.querySelector('.bot-grid');
 
-    if (result === 'hit') {
-      cell.style.backgroundColor = 'red';
-    } else if (result === 'miss') {
-      cell.style.backgroundColor = 'blue';
-    }
-  });
+    randomlyPlaceShips(bot.gameboard);
+    bot.gameboard.getCoordinates('.bot-grid', (x, y) => {
+      const result = user.attack(bot, x, y);
+      const cell = botGrid.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+
+      if (result === 'hit') {
+        cell.style.backgroundColor = 'red';
+      } else if (result === 'miss') {
+        cell.style.backgroundColor = 'blue';
+      }
+    });
+  }
+  
 }
